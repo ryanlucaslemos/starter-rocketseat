9440cd843e78a22a49ebd5ca26ec256b38c77a51
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-17 16:48:27 -0300

    Organiza a aplicação Separação da lógica (MVC)

--- node-api/server.js
+++ node-api/server.js
@@ -13,16 +13,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-//primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build fansmsa',
-        url: 'http://github.com'
-    })
-    return res.send('Hello teste');
-});
+//rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
