f6bbc80fb8f2d5871d91562f983a9e1f3980525f
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-17 16:23:38 -0300

    Inclui a criação de um produto no mongodb

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
+//iniciando o app
 const app = express();
 
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello teste');
+    Product.create({
+        title: 'React Native',
+        description: 'Build fansmsa',
+        url: 'http://github.com'
+    })
+    return res.send('Hello teste');
 });
 
 app.listen(3001);
