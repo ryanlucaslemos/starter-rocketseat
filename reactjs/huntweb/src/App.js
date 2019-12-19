aa7b4189cbcd0a8ba75df34204f27c3a01f86d1e
Ryan Lemos <ryanlucas.lemos@gmail.com>
2019-12-19 18:07:01 -0300

    Adiciona o header e a página principal

--- reactjs/huntweb/src/App.js
+++ reactjs/huntweb/src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+import './styles.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello </h1>
-      </div>
-    );
-  }
-}
+import Header from './components/Header';
+import Main from './pages/main'
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
+
 
 export default App;
